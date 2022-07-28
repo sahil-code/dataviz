e4be2b0ef8a0bfac69eb6395ef98aa480158c502
sahil <sahilagarwal4b@gmail.com>
2022-07-29 01:38:04 +0530

    completed week3.GIF

--- Week 3/Gif/Code/sketch.js
+++ Week 3/Gif/Code/sketch.js
@@ -2,7 +2,7 @@ function preload() {
   data = loadJSON("data.json");
   colors = loadJSON("colors.json");
   genreTotals = loadJSON("genrelist.json")
-  genreYearly = loadJSON("genrelist.json")
+  genreYearly = loadJSON("genrelist2.json")
   totalMovies = 3005
 }
 
@@ -15,39 +15,59 @@ function setup() {
   hmargin = 100;
   wmargin = 50;
   rightcolumn = 20;
-  printName();
-  maxmovt = 1000;
-  maxmovy = 100;
+  textAlign(CENTER, CENTER);
+  textFont("Trebuchet MS");
+  maxt = 1000;
+  maxy = 100;
   genreList = Object.keys(genreYearly);
   data[3006] = { Year: 9999 }
+  colorlist = ["Blue", "Red", "Yellow", "Green", "Purple"]
+
+  iterateMovie();
 }
 
 function draw() {
   background(colors.Grey.Light);
-  text(i, 25, 25);
-  text(data[i].Name, 60, 25);
-  text(data[i].Year, 300, 25);
+  text(data[i].Name, width / 2, 25);
 
-  drawLegend(0, maxmovy);
-  drawChart("Stroke", 0, genreYearly, maxmovy);
-  drawChart("Fill", 0, genreYearly, maxmovy);
+  drawLegend(0, maxy);
+  drawLegend(1, maxt);
 
-  drawLegend(1, maxmovt);
-  drawChart("Stroke", 1, genreTotals, maxmovt);
-  drawChart("Fill", 1, genreTotals, maxmovt);
+  push()
+  curcolor = colorlist[(data[i - 1].Year % 5)]
+  fill(colors[curcolor].Dark)
+  textSize(24)
+  drawChart("Stroke", 0, genreYearly, maxy, curcolor);
+  drawChart("Fill", 0, genreYearly, maxy, curcolor);
+  text(`Genre for ${data[i - 1].Year}`, dimxmid, dimy2 + 50)
+
+  fill(colors.Grey.Dark)
+  for (let y = data[i].Year; y >= data[0].Year; y--) {
+    drawChart("Fill", 1, genreTotals[y], maxt, colorlist[y % 5]);
+  }
+  text(`Genre for ${data[0].Year} to ${data[i - 1].Year}`, dimxmid, dimy2 + 50)
+  pop()
+
+  makeCredit()
 }
 
-function printName() {
+//iterate through the movielist by calling itself until i exceeds total
+function iterateMovie() {
   if (stopper == 0) {
     if (data[i].Year != data[i + 1].Year) {
       print("move!")
       n = Math.max(...Object.values(genreYearly));
+      curyear = data[i].Year
+      if (curyear != data[0].Year) {
+        genreTotals[curyear] = Object.assign({}, genreTotals[curyear - 1])
+        print("changing", genreTotals[curyear - 1], genreTotals[curyear])
+      }
       conductMovement();
     }
     genreYearly[data[i].Genre] += 1;
     if (i < totalMovies) {
       i += 1;
-      setTimeout(printName, timeoutperiod);
+      setTimeout(iterateMovie, timeoutperiod);
     }
     if (i == totalMovies + 1) {
       stopper = 1
@@ -55,13 +75,14 @@ function printName() {
   }
 }
 
+//activated every time year changes, to move from yearly data to annual
 function conductMovement() {
   for (let g = 0; g < genreList.length; g++) {
     stopper = 1;
     curgen = genreList[g];
     if (genreYearly[curgen] > n) {
       genreYearly[curgen] -= 1;
-      genreTotals[curgen] += 1;
+      genreTotals[curyear][curgen] += 1;
       print("doing")
       redraw();
     }
@@ -74,7 +95,7 @@ function conductMovement() {
     setTimeout(conductMovement, timeoutperiod);
   } else {
     stopper = 0;
-    printName();
+    iterateMovie();
   }
 }
 
@@ -97,15 +118,14 @@ function drawLegend(row, maxMovies) {
   push();
   textSize(10);
   strokeWeight(0.5);
+  stroke(31, 40, 51);
   for (let i = 0; i < 5; i++) {
     ypos = map(i, 0, 4, dimy2, dimy1);
 
     //horizontal lines
-    stroke(31, 40, 51);
     line(dimx1, ypos, dimx2, ypos);
 
     //numbers on left
-    stroke(31, 40, 51);
     textAlign(RIGHT, CENTER);
     mapvalue = map(i, 0, 4, 0, maxMovies);
     text(mapvalue, dimx1 - 10, ypos);
@@ -127,8 +147,9 @@ function drawLegend(row, maxMovies) {
   }
 }
 
-//draw either stroke or fill of chart
-function drawChart(type, row, dataobj, maxMovies) {
+//draw either stroke or fill of each chart
+function drawChart(type, row, dataobj, maxMovies, colorname) {
+  push()
   defineDimensions(row);
   beginShape();
 
@@ -147,15 +168,35 @@ function drawChart(type, row, dataobj, maxMovies) {
     );
   }
 
-  push();
   if (type == "Fill") {
-    fill(colors.Grey.Main);
+    fill(colors[colorname].MainFullOpa);
     strokeWeight(0);
   } else if ((type = "Stroke")) {
     noFill();
-    stroke(colors.Grey.Dark);
+    stroke(colors[colorname].Dark);
     strokeWeight(2);
   }
   endShape();
+  pop();
+}
+
+//draw the credit lines
+function makeCredit() {
+  push();
+  noStroke();
+  fill(0);
+  textSize(10);
+  textAlign(LEFT);
+  fill(colors.Grey.Dark);
+  text(
+    "Data Source: Bollywood Movies on Kaggle / IMDB",
+    wmargin,
+    height - hmargin / 3
+  );
+  text(
+    "Created by Sahil Agarwal using p5.js for IIP-Dataviz Week 3",
+    wmargin,
+    height - hmargin / 3 + 15
+  );
   pop();
 }
---
+++ Week 4/README.md
@@ -0,0 +1,3 @@
+# Week 4: DataViz with IIP
+
+![](2022-07-29-01-28-29.png)
